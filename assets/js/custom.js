d7cc6437d977941aa87ccad85317e04729585d13
vatsalvajani <58940747+vatsalvajani@users.noreply.github.com>
2023-04-22 14:49:05 +0530

    changes in animation and added scroll top button

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,4 +20,15 @@ $( document ).ready(function() {
         infinite: false,
         autoplay: false
     });
+
+    $.scrollUp({
+        scrollName: 'scrollUp', // Element ID
+        topDistance: '1110', // Distance from top before showing element (px)
+        topSpeed: 2000, // Speed back to top (ms)
+        animation: 'slide', // Fade, slide, none
+        animationInSpeed: 300, // Animation in speed (ms)
+        animationOutSpeed: 300, // Animation out speed (ms)
+        scrollText: '<span class="up-arrow"></span>', // Text for element
+        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+    });
 });
